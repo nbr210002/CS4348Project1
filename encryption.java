2fde37ccf80d689eb49411d9281781842836fe0b
NoorhanR <rashidnoorhan@gmail.com>
2025-10-14 13:35:36 -0500

    10/14/2025 1:35PM
    I have started working on the encryption.java file. So far I have worked on the Encryption file, and I am planning on implementing a cipher.
    
    My thoughts: I am implementing a cipher to encrypt the text. I now need to work on decrypting the text; the decrypt portion of the code.
    
    Plan: Right now, the plan has shifted to work on each file one at a time. I plan to get the basics down, and refer back to the pseudocode I have written down. The pseudocode may end up being more of what I need the code to accomplish, rather than follow it down to a tee.

--- encryption.java
+++ encryption.java
@@ -1,3 +1,32 @@
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class Encryption
+{
+    int key = 3; // places to shift; beginning, middle, end
+
+    //encryption method
+    public String encrypt(String sentence)
+    {
+        StringBuilder cipher = new StringBuilder();
+        for (char x : sentence.toCharArray())
+        {
+            if(Character.isLetter(x))
+            {
+                char base = Character.isUpperCase(x) ? 'A' : 'a'; // see if uppercase vs lowercase
+                char shift = (char) ((x - base + key) % 26 + base); // 26 letters in alphabet
+                cipher.append(shift);
+            }
+            else
+            {
+                cipher.append(x);
+            }
+        }
+        return cipher.toString();
+    }
+}
+
 // Pseudocode for what to do... maybe
 /*
 read from stdin
@@ -11,5 +40,4 @@ else if command == DECRYPT
     if key == null then print ERROR
     else decrypted
         print result, decrypted
-
  */
