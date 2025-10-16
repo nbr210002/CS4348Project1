fad75f01bedb42bb2d85d0ab26c0d744baaa895e
NoorhanR <rashidnoorhan@gmail.com>
2025-10-16 17:47:28 -0500

    10/16/2025 5:34PM I have worked on my encryption.java again. I realized I was implementing the cipher incorrectly, so I had to redo my code for the encryption. My thoughts: I have written the encryption and decryption methods. I have made sure I use the correct, Vigenere Cipher, this time. I made sure my code follows my pseudocode that I have previously written. However, I think that I will have the "stdin" line read and split in a different file. Plan: I plan to move on to work on the Logger.java file and then the Driver.java file. I also think I am going to have my Driver.java file read the "stdin" and the splitting into command and argument instead.

--- encryption.java
+++ encryption.java
@@ -1,43 +1,105 @@
 import java.lang.*;
 import java.io.*;
 import java.util.Scanner;
+import java.util.*;
 
 public class Encryption
 {
-    int key = 3; // places to shift; beginning, middle, end
+    // Store passkey
+    String passkey = null;
 
-    //encryption method
-    public String encrypt(String sentence)
+    // Check if the String has letters only
+    private static boolean lettersOnly(String x)
     {
-        StringBuilder cipher = new StringBuilder();
-        for (char x : sentence.toCharArray())
+        return x != null && x.matches("[A-Za-z]+");
+    }
+
+    // Normalize String
+    public static String setToNormal(String x)
+    {
+        return x.toUpperCase();
+    }
+
+    // Set passkey if it is valid
+    public void setPassKey(String key)
+    {
+        if(lettersOnly(key))
         {
-            if(Character.isLetter(x))
+            passkey = setToNormal(key);
+            System.out.println("RESULT Passkey set");
+        }
+        else
+        {
+            System.out.println("ERROR Passkey must contain only letters.");
+        }
+    }
+
+    //Encrypt using passkey
+    public String encrypt(String text)
+    {
+        if (passkey == null)
+        {
+            return "ERROR Password not set";
+        }
+        return "RESULT " + vigenereCipherEncrypt(text, passkey);
+    }
+
+    // Encryption using Vigenere Cipher
+    private static String vigenereCipherEncrypt(String text, String key)
+    {
+        StringBuilder result = new StringBuilder();
+        text = setToNormal(text);
+        key = setToNormal(key);
+
+        int keyIndex = 0;
+        for (char ch : text.toCharArray())
+        {
+            if (Character.isLetter(ch))
             {
-                char base = Character.isUpperCase(x) ? 'A' : 'a'; // see if uppercase vs lowercase
-                char shift = (char) ((x - base + key) % 26 + base); // 26 letters in alphabet
-                cipher.append(shift);
+                int shift = key.charAt(keyIndex % key.length()) - 'A';
+                char encrypted = (char) ((ch - 'A' + shift)% 26 + 'A');
+                result.append(encrypted);
+                keyIndex++;
             }
             else
             {
-                cipher.append(x);
+                result.append(ch);
             }
         }
-        return cipher.toString();
+        return result.toString();
+    }
+
+    // Decrypt using the passkey
+    public String decrypt(String text)
+    {
+        if (passkey == null)
+        {
+            return "ERROR Password not set";
+        }
+        return "RESULT " + vigenereCipherDecrypt(text, passkey);
+    }
+
+    // Decrption using Vigenere Cipher
+    private static String vigenereCipherDecrypt(String text, String key)
+    {
+        StringBuilder result = new StringBuilder();
+        text = setToNormal(text);
+        key = setToNormal(key);
+        int keyIndex = 0;
+        for (char ch : text.toCharArray())
+        {
+            if (Character.isLetter(ch))
+            {
+                int shift = key.charAt(keyIndex % key.length()) - 'A';
+                char decrypted = (char) ((ch - 'A' - shift + 26) % 26 + 'A');
+                result.append(decrypted);
+                keyIndex++;
+            }
+            else
+            {
+                result.append(ch);
+            }
+        }
+        return result.toString();
     }
 }
-
-// Pseudocode for what to do... maybe
-/*
-read from stdin
-split line into command and argument
-
-if command == quit then break
-else comand == PASSKEY then print result
-else command == ENCRYPT then print ERROR
-
-else if command == DECRYPT
-    if key == null then print ERROR
-    else decrypted
-        print result, decrypted
- */
